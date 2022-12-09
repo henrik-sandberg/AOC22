4916372e3eeca0a10cf06460b1aa4a2efc44531d
Henrik Sandberg <48967132+henrik-sandberg@users.noreply.github.com>
2022-12-10 00:50:51 +0100

    Indentation

--- day05.go
+++ day05.go
@@ -69,9 +69,9 @@ func getResult(stacks map[int]string) string {
 
 func reverse(s string) string {
 	rns := []rune(s)
-    for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
-        rns[i], rns[j] = rns[j], rns[i]
-    }
-    return string(rns)
+	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
+		rns[i], rns[j] = rns[j], rns[i]
+	}
+	return string(rns)
 }
 
